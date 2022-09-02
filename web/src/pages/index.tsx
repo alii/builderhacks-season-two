9596aeb43623d49ed6fc1e25d30fae09b798ea7c
Alistair Smith <hi@alistair.sh>
2022-09-02 18:47:40 +0200

    feat: prettier

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,8 @@
-import Internet from "../components/internet";
+import Internet from '../components/internet';
 
-export default () => <div><h1>hi</h1><Internet/></div>;
+export default () => (
+	<div>
+		<h1>hi</h1>
+		<Internet />
+	</div>
+);
