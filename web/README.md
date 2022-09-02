98c1435387a9f9530ff0d80801220f026c169bfe
AlfieRan <alfie.ranstead@outlook.com>
2022-09-02 17:45:13 +0100

    components folder

--- web/README.md
+++ web/README.md
@@ -1,1 +1,3 @@
 # web
+
+I love web development - Alfie Ranstead
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,1 +1,3 @@
-export default () => <h1>hi</h1>;
+import Internet from "../components/internet";
+
+export default () => <div><h1>hi</h1><Internet/></div>;
