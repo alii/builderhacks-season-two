159edcee204ed1d1de17eae21f735f6c51edb379
AlfieRan <alfie.ranstead@outlook.com>
2022-09-03 17:29:27 +0100

    feat: unsafe bad code but messages work so W

--- web/src/pages/api/send-message.ts
+++ web/src/pages/api/send-message.ts
@@ -8,18 +8,27 @@ const schema = z.object({
 
 export default api({
 	async POST({req, ctx}) {
+		console.log('Received a message to send.');
 		const token = await ctx.getToken();
 		const body = schema.parse(req.body);
 
 		const talkingTo = await ctx.talkingTo.get(token.id);
+		console.log('Figured out who we are talking to.');
 
 		if (!talkingTo) {
 			throw new NextkitError(400, 'You are not talking to anyone');
 		}
 
+		console.log(
+			'Sending message to ',
+			talkingTo,
+			' with content ',
+			body.content,
+		);
 		await ctx.utils.hop.publishDirectMessage(talkingTo, 'CHAT_EVENT', {
 			type: 'user',
 			content: body.content,
 		});
+		console.log('finished sending message');
 	},
 });
--- web/src/pages/chat.tsx
+++ web/src/pages/chat.tsx
@@ -1,31 +1,51 @@
 import {useState} from 'react';
 import {fetcher} from '../client/fetcher';
+import {useDirectMessage} from '@onehop/react';
+import {useQuery} from '../hooks/query';
+import LeapAPI from './api/leap';
 
 export default function Chat() {
+	const {data: token} = useQuery<typeof LeapAPI>('/api/leap');
 	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState<string[]>([]);
+
+	const user_token = token?.id || '';
+	console.log('receiving messages as token: ', user_token);
+
+	useDirectMessage('CHAT_EVENT', msg => {
+		console.log('Received message: ', msg);
+		setMessages([...messages, msg.content]);
+	});
 
 	return (
 		<div className={'p-3'}>
-			<input
-				type="text"
-				value={message}
-				placeholder="Message"
-				onChange={e => setMessage(e.target.value)}
-			/>
+			<div>
+				<input
+					type="text"
+					value={message}
+					placeholder="Message"
+					onChange={e => setMessage(e.target.value)}
+				/>
 
-			<button
-				type="button"
-				onClick={async e => {
-					e.preventDefault();
+				<button
+					type="button"
+					onClick={async e => {
+						e.preventDefault();
 
-					await fetcher('/api/send-message', {
-						method: 'POST',
-						body: {content: message},
-					});
-				}}
-			>
-				send
-			</button>
+						await fetcher('/api/send-message', {
+							method: 'POST',
+							body: {content: message},
+						});
+					}}
+				>
+					send
+				</button>
+			</div>
+			<div>
+				{messages.map((message, index) => (
+					<div key={index}>{message}</div>
+				))}
+			</div>
 		</div>
 	);
 }
